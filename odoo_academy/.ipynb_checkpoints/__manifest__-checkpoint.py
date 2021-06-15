844f6095e47f6d01b9534007c8e001df97183841
hisham yass <hisham@odoo.com>
2021-06-15 12:49:32 +0000

    First Commit

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,21 @@
+# _*_ coding: utf-8 _*_
+
+{
+    
+    'name': 'Odoo Academy Hisham',
+    'description': """
+        My First Module
+        Course
+        Sessions
+        Attendees       
+     
+    """,
+    'author': 'hisham',
+    'website': 'http://yassin.com',
+    'category':'Training',
+    'version': '0.1',
+    'depends':['base'],
+    'data': [],
+    'demo':[],
+    
+}
